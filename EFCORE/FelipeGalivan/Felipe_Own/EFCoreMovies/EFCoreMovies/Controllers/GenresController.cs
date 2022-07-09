8fafacb241f336e3ddec78bba801dd57b613ff42
Balazs Rigo <rigo.balazs86@gmail.com>
2022-07-09 16:21:23 +0200

    Section 3 in progress! (37)

---
+++ EFCORE/FelipeGalivan/Felipe_Own/EFCoreMovies/EFCoreMovies/Controllers/GenresController.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreMovies.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController :ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public GenresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Genre>> Get()
+        {
+            return await context.Genres.ToListAsync();
+        }
+    }
+}
